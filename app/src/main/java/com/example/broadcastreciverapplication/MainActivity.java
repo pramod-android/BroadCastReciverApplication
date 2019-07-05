3124d0b2e405a1eec8ae6269650771680b9ccbda
pramod-android <pramodpatil4212@gmail.com>
2019-07-05 10:00:54 +0530

    get text from user

--- app/src/main/java/com/example/broadcastreciverapplication/MainActivity.java
+++ app/src/main/java/com/example/broadcastreciverapplication/MainActivity.java
@@ -4,13 +4,20 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText=(EditText)findViewById(R.id.editText);
+
+
+
+
 //        Intent intent = new Intent("my.action.string");
 //        intent.putExtra("extra", "My string"); // phoneNo is the value to sent
 //        sendBroadcast(intent);
@@ -18,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void SendBroadcast(View view) {
         Intent intent = new Intent("my.action.string");
-        intent.putExtra("extra", "My string"); // phoneNo is the value to sent
+        intent.putExtra("extra", editText.getText().toString()); // phoneNo is the value to sent
         sendBroadcast(intent);
 
     }
